f29a8920f62394991d2ef29a1fc1dd45edf1bd29
NameLloyd14a <91308056+NameLloyd14a@users.noreply.github.com>
2024-02-23 15:40:17 +0100

    Update index.php
    Tried out navbar (not finished)

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
 <style>
 ul {
+  position: fixed;
+  top: 0;
+  width: 100%;
   list-style-type: none;
   margin: 0;
   padding: 0;
@@ -12,33 +15,45 @@ ul {
 
 li {
   float: left;
+  border-right: 1px solid #bbb;
 }
-
+/* Remove the border from the last element */
+li:last-child {
+  border-right: none;
+}
+/* styling for buttons */
 li a {
   display: block;
   color: white;
   text-align: center;
-  padding: 14px 16px;
+  padding: 16px 30px;
   text-decoration: none;
+  font-size: 20px;
 }
-
+/* defines the color of the inactive blocks */
 li a:hover:not(.active) {
   background-color: #111;
 }
-
+/* defines the color of the active blocks */
 .active {
   background-color: #04AA6D;
+}
+/* Style for the home button */
+#home-icon {
+  height: 30px; /* Adjust the height of the image */
 }
 </style>
 </head>
 <body>
 
 <ul>
-  <li><a class="active" href="#home">Home</a></li>
+  <li><a class="active" href="#home"><img id="home-icon" src="website_logo.png" alt="Home"></a></li>
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
+  <li style="float:right"><a href="#about">About</a></li>
 </ul>
 
 </body>
 </html>
+
+
