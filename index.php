114970bcea60d40075a47cc436901ada47e7cd65
Lukiluck8400 <160834438+Lukiluck8400@users.noreply.github.com>
2024-02-29 12:55:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,47 +3,81 @@
 <head>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 <style>
   body {
    font-family: "Open Sans", sans-serif;
    background-color: whitesmoke;
-    
-} 
+   
+}
+  .image {
+    width : 70%
+ 
+  }
   .logo {
-    width : 100%
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width : 20% ;
+ 
   }
   .hedline {
      
   color : whitesmoke;
   }
   .navbar {
+    background-color: black ;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-between ;
     align-items: center;
+  }
+  .headline {
+    color:aquamarine ;
+    font-weight : bold;
+ 
+  }
+  .menu-links {
+    color: whitesmoke;
+    text-decoration: none;
+   
+   
+  }
+  a:hover {
+    text-decoration: underline;
+    color:aquamarine;
+    margin : left
+  }
+  .menu {
+    width : 50% ;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: right ;
+    align-items: center;
+    font-weight: bold;
   }
 </style>
 </head>
 <body>
 <div class ="navbar">
-  <div>
-    <h1 class = "headline"> 
+  <div class = "logo">
+    <div >
+      <img class ="image" src="website_logo.png">
+    </div>
+ 
+    <h1 class = "headline">
     Zugvögel
     </h1>
   </div>
-
- <img class ="logo" src="website_logo.png">
-  <div>
-
-    <a href = ''> Home</a>
-    <a href = ''> News </a>
-    <a href = ''> Contact </a>
-    <a href = ''> About </a>
+ 
+ 
+  <div class = "menu" >
+ 
+    <a class = "menu-links" href = 'index.php'> Home</a>
+    <a class = "menu-links" href = ''> News </a>
+    <a class = "menu-links" href = ''> Contact </a>
+    <a class = "menu-links" href = ''> About </a>
   </div>
  
 </div>
 </body>
 </html>
-
-
