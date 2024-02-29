eeb74292e08738338569f411c853916f42c54c47
Bleili <160834821+Bleili@users.noreply.github.com>
2024-02-29 10:40:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"> 
 <style>
-ul {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
+  body {
+   font-family: "Open Sans", sans-serif;
+   background-color: whitesmoke;
+    
+} 
+  .logo {
+    width : 100%
 
-li {
-  float: left;
-  border-right: 1px solid #bbb;
-}
-/* Remove the border from the last element */
-li:last-child {
-  border-right: none;
-}
-/* styling for buttons */
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 16px 30px;
-  text-decoration: none;
-  font-size: 20px;
-}
-/* defines the color of the inactive blocks */
-li a:hover:not(.active) {
-  background-color: #111;
-}
-/* defines the color of the active blocks */
-.active {
-  background-color: #04AA6D;
-}
-/* Style for the home button */
-#home-icon {
-  height: 30px; /* Adjust the height of the image */
-}
-.navbar {
-  display : flex;
-  justify-content: space-between;
-}
+  }
+  .hedline {
+     
+  color : whitesmoke;
+  }
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
 </head>
 <body>
-
-<ul> 
-  <div class = 'navbar'> 
-
-    <li><a class="active" href="#home"><img id="home-icon" src="website_logo.png" alt="Home"></a></li>
+<div class ="navbar">
   <div>
-    <li><a href="#news">News</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li style="float:right"><a href="#about">About</a></li>
+    <h1 class = "headline"> 
+    Zugvögel
+    </h1>
   </div>
 
-</ul>
+ <img class ="logo" src="website_logo.png">
+  <div>
 
+    <a href = ''> Home</a>
+    <a href = ''> News </a>
+    <a href = ''> Contact </a>
+    <a href = ''> About </a>
+  </div>
+ 
+</div>
 </body>
 </html>
 
