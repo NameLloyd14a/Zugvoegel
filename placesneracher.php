1ef498e9df69509f773487e2381d89f13573a4ab
Lukiluck8400 <160834438+Lukiluck8400@users.noreply.github.com>
2024-03-12 17:07:11 +0100

    Update placesneracher.php

--- placesneracher.php
+++ placesneracher.php
@@ -50,15 +50,16 @@
         }
 
         .container {
-            background-color: gray;
-            height: 100%; /* Use 100% height to occupy the entire container */
+            background-color: whitesmoke;
+            height: 50%; /* Use 100% height to occupy the entire container */
             width: 67%;
             display: flex;
             flex-direction: column; /* Arrange items vertically */
             justify-content: center;
+            font-size: 18px;
 
             position: absolute;
-            top: 1050px;
+            top: 650px;
             left: 50%;
             transform: translate(-50%, -50%);
         }
@@ -67,10 +68,11 @@
             display: flex;
             justify-content: center;
             padding: 20px;
+            margin-top:800px;
         }
 
         p {
-            color: white;
+            color: black
             font-family: sans-serif;
         }
 
@@ -170,12 +172,36 @@
         </div>
     </div>
 
-    <h1 class='titel' style='text-align : center;' >Ein Jahr im Neeracherried</h1>
+    <h1 class='titel' style='text-align : center;' font-size: 30px; >Neeracherried</h1>
 
     <div class='container'>
-        <p style='text-align : center;'>
-            <!-- Your text content here -->
-        </p>
+    <p style= 'text-align : left;'> Das BirdLife-Naturzentrum Neeracherried liegt am Rand eines der letzten grossen Flachmoore der Schweiz. Dank einem Steg ins Ried und zwei Beobachtungshütten lässt sich die vielfältige Natur – speziell Vögel – hautnah erleben: Im Ried brüten Kiebitz, Eisvogel und Lachmöwe, und hier rasten Zugvögel wie Grünschenkel oder Silberreiher.
+        Im Gebäude gibt es eine interaktive Ausstellung zum Lebensraum Ried, die auch für Kinder geeignet ist. Eine Videoschau, zwei Naturpfade und Sonderausstellungen - aktuell zu "Geheimnisvolle Unterwasserwelt" mit lebendigen Kleintieren in Aquarien – runden das attraktive Angebot ab.
+        Das BirdLife-Naturzentrum Neeracherried ist nicht nur für Einzelbesucher offen, sondern auch für alle Arten von Gruppen, von Familien über Vereine, und Firmen bis zu Verwaltungen. Für Schulen gibt es spezielle Materialien.
+        <br><br><br>
+        Ihr Anlass
+        Firmenausflug mit Apéro, Familienfest mit Führung in die Natur, Hochzeits-Überraschungsausflug: Dies alles ist im BirdLife-Naturzentrum möglich. Wir beraten Sie gerne.
+        <br>
+        Schulen und Kinder
+        Schulklassen jeden Alters, Lehrerkollegien und Kindergruppen sind im BirdLife-Naturzentrum Neeracherried speziell willkommen. Wir veranstalten stufengerechte Führungen vom Kindergarten- bis zum Universitätsniveau. Für alle Schulstufen (Unterstufe, Mittelstufe, Oberstufe, Kantonsschule Oberstufe) gibt es auf der Webpage ein umfangreiches Vor- und Nachbereitungsdossier für die Führung im Neeracherried. Die wichtigsten Themen sind Vögel und Vogelzug, Kleintiere im Teich sowie Naturschutz - wir passen das Programm gerne den spezifischen Voraussetzungen der Gruppen an.
+        <br><br><br>
+        Öffnungszeiten<br>
+        April bis Oktober<br>
+        Mi 14–20 Uhr<br>
+        Sa 10–18 Uhr<br>
+        So und Feiertage 8–18 Uhr
+        <br><br>
+        November bis März
+        2. Sonntag des Monats 9–16 Uhr
+        <br><br><br>
+        Kontakt:
+        BirdLife-Naturzentrum Neeracherried
+        Dielsdorferstrasse 13, Postfach
+        8173 Neerach
+        Tel. 044 858 13 00
+        birdlife.ch/naturzentrum-neeracherried</p>
+    </div>
+
 
         <div class="titelbild">
             <video autoplay controls width="70%">
